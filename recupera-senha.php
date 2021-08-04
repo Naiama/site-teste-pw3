c5e82d4274b9ed61ac4422e847fda2b3078fe194
Naiama Vagula <naiamavagula@gmail.com>
2021-08-03 23:45:41 -0300

    Adicionado os arquivos da primeira atividade

---
+++ recupera-senha.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<!-- saved from url=(0060)https://getbootstrap.com/docs/4.1/examples/starter-template/ -->
+<html lang="en"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="favicon/pw3.ico">
+
+    <title>PW3</title>
+
+    <link rel="canonical" href="https://getbootstrap.com/docs/4.1/examples/starter-template/">
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/starter-template.css" rel="stylesheet">
+
+    <!-- FONTAWESOME -->
+    <script src="https://kit.fontawesome.com/77f3dd62a7.js"> crossorigin="anonymous"</script>
+  </head>
+
+  <body class="bg-secondary">
+
+
+
+    <main role="main" class="container">
+
+      <div class="row">
+          <div class="col-sm-4 offset-sm-4 border bg-white" >
+              <h1 class="text-center">
+                  <a href="index.php">PW3</a>
+              </h1>
+             
+              <form action="" method="">
+
+               <div class="input-group mb-3">
+                <div class="input-group-prepend">
+                 <span class="input-group-text" id="basic-addon1"><i class="fas fa-envelope"></i></span>
+                </div>
+                 <input type="mail" name="email" class="form-control" placeholder="E-mail" aria-label="E-mail" aria-describedby="basic-addon1">
+               </div>
+
+               <div class="form-group text-right">
+                   <button class="btn btn-primary">Recuperar</button>
+               </div>
+
+               <p>
+                   <a href="login.php">Fazer login</a>
+               </p>
+
+              </form>
+          </div>
+      </div>
+
+    </main><!-- /.container -->
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="js/jquery-3.3.1.slim.min.js.download"></script>
+   
+    
+    <script src="js/bootstrap.min.js.download"></script>
+  
+
+</body></html>
